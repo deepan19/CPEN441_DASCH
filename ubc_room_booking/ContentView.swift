a3e75fa963d2d6e14a3b87e1a3000437bf5f709a
Deepan  Chakravarthy <kdeepan240@gmail.com>
2025-03-18 22:22:00 -0700

    Initial Commit

---
+++ ubc_room_booking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ubc_room_booking
+//
+//  Created by Deepan Chakravarthy on 2025-03-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ubc_room_booking/ubc_room_bookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ubc_room_bookingApp.swift
+//  ubc_room_booking
+//
+//  Created by Deepan Chakravarthy on 2025-03-18.
+//
+
+import SwiftUI
+
+@main
+struct ubc_room_bookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
